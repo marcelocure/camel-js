182c5ae47c0daaf4cce39b82f9a0fb60e93a20e8
Marcelo Cure <marcelo.cure@ilegra.com>
2017-07-27 20:35:14 -0300

    fix lint issues

--- retry.js
+++ retry.js
@@ -1,54 +1,56 @@
-const R = require('ramda')
-var exception = {},
-    exceptions = [],
-    thisObjs = {
-        onException: onException,
-        getExceptions: () => exceptions,
-        retryRepetitions: retryRepetitions,
-        retryDelay: retryDelay,
-        fallbackProcessor: fallbackProcessor,
-        end: end,
-        getStrategy: getStrategy
-    },
-    defaultStrategy = {
-        fallbackProcessor : defaultFallbackProcessor,
-        retryRepetitions : 2,
-        retryDelay : 500
-    }
+const R = require('ramda');
+
+const defaultStrategy = {
+  fallbackProcessor: defaultFallbackProcessor,
+  retryRepetitions: 2,
+  retryDelay: 500,
+};
+const thisObjs = {
+  onException,
+  getExceptions: () => exceptions,
+  retryRepetitions,
+  retryDelay,
+  fallbackProcessor,
+  end,
+  getStrategy,
+};
+
+let exception = {};
+let exceptions = [];
 
 function onException(routeName) {
-    exception.routeName = routeName
-    exception.fallbackProcessor = defaultFallbackProcessor
-    return thisObjs
+  exception.routeName = routeName;
+  exception.fallbackProcessor = defaultFallbackProcessor;
+  return thisObjs;
 }
 
 function retryRepetitions(value) {
-    exception.retryRepetitions = value
-    return thisObjs
+  exception.retryRepetitions = value;
+  return thisObjs;
 }
 
 function retryDelay(value) {
-    exception.retryDelay = value
-    return thisObjs
+  exception.retryDelay = value;
+  return thisObjs;
 }
 
-function fallbackProcessor(func){
-    exception.fallbackProcessor = func
-    return thisObjs
+function fallbackProcessor(func) {
+  exception.fallbackProcessor = func;
+  return thisObjs;
 }
 
 function end() {
-    exceptions.push({routeName: exception.routeName, strategy: exception})
+  exceptions.push({ routeName: exception.routeName, strategy: exception });
 }
 
 function defaultFallbackProcessor(exchange) {
-    console.log(`defaultFallbackProcessor exchange: [${JSON.stringify(exchange)}]`)
-    return Promise.reject(exchange)
+  console.log(`defaultFallbackProcessor exchange: [${JSON.stringify(exchange)}]`);
+  return Promise.reject(exchange);
 }
 
 function getStrategy(routeName) {
-    var exceptionStrategy = R.find(R.propEq('routeName', routeName))(exceptions);
-    return !exceptionStrategy ? defaultStrategy : exceptionStrategy.strategy
+  const exceptionStrategy = R.find(R.propEq('routeName', routeName))(exceptions);
+  return !exceptionStrategy ? defaultStrategy : exceptionStrategy.strategy;
 }
 
-module.exports = thisObjs
+module.exports = thisObjs;
